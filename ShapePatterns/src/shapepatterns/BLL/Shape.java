1515f4046b9e7f9116a6ca0ff2fe4ca50a483ef3
Péter Sebők <sebok.peter97@gmail.com>
2017-11-05 22:11:14 +0100

    Revert "First implementation of the Shape() class"
    This reverts commit e59e8d07d215c895daeb01351a732ffe5541d753.

--- ShapePatterns/src/shapepatterns/BLL/Shape.java
+++ ShapePatterns/src/shapepatterns/BLL/Shape.java
@@ -18,27 +18,10 @@ import javafx.scene.paint.Color;
 public class Shape implements Drawable
 {
     private List<Point> points;
-    private String name;
-    
+
     public Shape()
     {
         this.points = new ArrayList();
-    }
-    
-    public Shape(String name)
-    {
-        this.points = new ArrayList();
-        this.name = name;
-    }
-    
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-    
-    public String getName()
-    {
-        return this.name;
     }
     
     public void addPoint(Point p)
@@ -46,51 +29,16 @@ public class Shape implements Drawable
         points.add(p);
     }
     
-    private double[] getXCoordinates()
-    {
-        double[] xCoords = new double[points.size()];
-        
-        for (int i = 0; i < points.size(); i++)
-        {
-            xCoords[i] = points.get(i).getX();
-        }
-        
-        return xCoords;
-    }
-    
-        private double[] getYCoordinates()
-    {
-        double[] yCoords = new double[points.size()];
-        
-        for (int i = 0; i < points.size(); i++)
-        {
-            yCoords[i] = points.get(i).getY();
-        }
-        
-        return yCoords;
-    }
-    
-    /**
-     * Draw a line from each point to the next
-     * @param context The lines will be drawn on this 
-     */
     @Override
     public void draw(GraphicsContext context)
     {
         context.setFill(Color.BLACK);
         context.setStroke(Color.BLACK);
         
-        /*
         for (int i = 0; i < points.size() - 1; i++)
         {
             context.strokeLine(points.get(i).getX(), points.get(i).getY(), points.get(i + 1).getX(), points.get(i + 1).getY());
         }
-        
-        
-        context.strokeLine(points.get(points.size()  - 1).getX(), points.get(points.size() - 1).getY(), points.get(0).getX(), points.get(0).getY()); //Draw a line from the last point to the first
-        */
-        
-        context.fillPolygon(getXCoordinates(), getYCoordinates(), points.size());
     }
     
 }
--- ShapePatterns/src/shapepatterns/GUI/controller/DrawWindowController.java
+++ ShapePatterns/src/shapepatterns/GUI/controller/DrawWindowController.java
@@ -6,10 +6,7 @@
 package shapepatterns.GUI.controller;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,11 +14,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.util.StringConverter;
+import javafx.scene.paint.Color;
 import shapepatterns.BLL.Point;
 import shapepatterns.BLL.Shape;
 /**
@@ -32,6 +28,7 @@ public class DrawWindowController implements Initializable
 {
     
     private Label label;
+    @FXML
     private ChoiceBox<?> cBoxShapeSelect;
     @FXML
     private TextField txtFieldSize;
@@ -40,6 +37,8 @@ public class DrawWindowController implements Initializable
     @FXML
     private Button btnAdd;
     @FXML
+    private ChoiceBox<?> cBoxDrawStrategy;
+    @FXML
     private Button btnDraw;
     @FXML
     private Button btnClear;
@@ -47,25 +46,13 @@ public class DrawWindowController implements Initializable
     private Canvas canvas;
     
     private GraphicsContext context;
-    @FXML
-    private ComboBox<Shape> comboBxShapeSelect;
-    @FXML
-    private ComboBox<?> comboBxDrawStrategy;
-    
-    private List<Shape> shapes;
     
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        shapes = new ArrayList<>();
         context = canvas.getGraphicsContext2D();
-        setUpComboBox();
     }    
 
-    /**
-     * Draws the selected shape onto the canvas
-     * @param event 
-     */
     @FXML
     private void btnDrawClick(ActionEvent event)
     {
@@ -73,62 +60,20 @@ public class DrawWindowController implements Initializable
         double x = canvas.getWidth()/2;
         double y = canvas.getHeight()/2;
         Point p = new Point(x, y);
-        Point p2 = new Point(x+50, y+50);
-        Point p3 = new Point(x-50, y+50);
+        Point p2 = new Point(x+100, y+100);
         
-        Shape s = new Shape("Triangle");
+        Shape s = new Shape();
         s.addPoint(p);
         s.addPoint(p2);
-        s.addPoint(p3);
         
         s.draw(context);
-        if (!shapes.contains(s))
-        {
-            shapes.add(s);
-        }
-        updateComboBox();
+        
     }
     
-    /**
-     * Clears the canvas
-     * @param event 
-     */
+    
     @FXML 
     private void btnClearClick(ActionEvent event)
     {
         context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-    }
-    
-    /**
-     * Sets up the comboBox
-     */
-    private void setUpComboBox()
-    {
-        comboBxShapeSelect.setItems(FXCollections.observableArrayList(shapes));
-        comboBxShapeSelect.setConverter(new StringConverter<Shape>()
-        {
-            @Override
-            public String toString(Shape object)
-            {
-                return object.getName();
-            }
-
-            @Override
-            public Shape fromString(String string)
-            {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-
-        });
-        shapes.add(new Shape("Name"));
-        updateComboBox();
-    }
-    
-    /**
-     * Updates the content of the combo box. Used when a new item is added to the list
-     */
-    private void updateComboBox()
-    {
-        comboBxShapeSelect.setItems(FXCollections.observableArrayList(shapes));
     }
 }
